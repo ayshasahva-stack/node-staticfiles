78a396d7f2d57186af95a46c386de82a959af626
aysha sahva <ayshasahva@gmail.com>
2026-07-11 23:13:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const fs = require('fs')
+
+
+const server = http.createServer((req, res) => {
+    let filePath
+    let contentType
+
+    if (req.url === '/') {
+        filePath = './public/index.html'
+        contentType: 'text/html'
+    }
+    else if (req.url === '/index.css') {
+        filePath = './public/index.css';
+        contentType: 'text/css'
+    }
+    else {
+        res.writeHead(404, { 'contentType': 'text/plain' });
+        res.end('file not found')
+        return
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { "content-type": 'text/plain' })
+            res.end(' reading failed')
+            return
+        }
+        res.writeHead(200, { "content-type": contentType })
+        res.end(data)
+
+    })
+})
+server.listen(3000, () => console.log('server running...'))
