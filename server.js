d220f655e90d3be07800899a49d8e05659a6a826
aysha sahva <ayshasahva@gmail.com>
2026-07-13 10:05:40 +0530

    second commit

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ const server = http.createServer((req, res) => {
 
     if (req.url === '/') {
         filePath = './public/index.html'
-        contentType: 'text/html'
+        contentType= 'text/html'
     }
     else if (req.url === '/index.css') {
         filePath = './public/index.css';
-        contentType: 'text/css'
+        contentType= 'text/css'
     }
     else {
-        res.writeHead(404, { 'contentType': 'text/plain' });
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('file not found')
         return
     }
 
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            res.writeHead(500, { "content-type": 'text/plain' })
+            res.writeHead(500, { "Content-Type": 'text/plain' })
             res.end(' reading failed')
             return
         }
-        res.writeHead(200, { "content-type": contentType })
+        res.writeHead(200, { "Content-Type": contentType })
         res.end(data)
 
     })
